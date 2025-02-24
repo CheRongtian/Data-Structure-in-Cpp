2e323b398d65a62bca025b44f073f43fa77ce96b
CheRongtian <145754269+CheRongtian@users.noreply.github.com>
2025-02-24 00:44:38 -0600

    change some, push first, TBC

--- array-operations.cpp
+++ array-operations.cpp
@@ -37,6 +37,25 @@ void insert(struct Array *array, int index, int x)
     }
 }
 
+void del(struct Array *array, int index)
+{
+    int x = array -> A[index];
+    for (int i = index; i < (array -> length) - 1; i++)
+        array -> A[i] = array -> A[i+1];
+    array -> length --;
+}
+
+int linear_search(struct Array *array, int x)
+{
+    int i;
+    for(i = 0; i < array ->length; i++)
+    {
+        if(array ->A[i] == x)
+            return i;   // successful
+    }
+    return -1; // unsuccesful
+}
+
 int main()
 {
     struct Array A = {{2, 3, 4, 5, 6}, 20, 5};
@@ -44,5 +63,13 @@ int main()
     display(A);
     insert(&A, 2, 100);
     display(A);
+    del(&A, 2);
+    display(A);
+    
+    int result = linear_search(&A, 100);
+    if(result != -1)
+        cout << "The index of the targeted value is: " << result << endl;
+    else
+        cout << "There is no such value in the array." << endl;
     return 0;
 }
