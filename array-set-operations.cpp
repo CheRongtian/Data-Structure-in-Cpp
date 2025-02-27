62ef2c5ac2c263bdce0748b5afa9bba470603970
CheRongtian <145754269+CheRongtian@users.noreply.github.com>
2025-02-27 00:55:05 -0600

    Remain: Intersection and Difference

--- array-set-operations.cpp
+++ array-set-operations.cpp
@@ -37,19 +37,54 @@ void union_unsorted(struct Array *array, struct Array *array2, struct Array *arr
                 break;
             }
         }
-    if(!overlap)
+        if(!overlap)
+        {
+            array3->A[k++] = array2->A[j];
+        }
+    }
+    array3 ->length = k;
+}
+
+void union_sorted(struct Array *array, struct Array *array2, struct Array *array3)
+{
+    int i = 0;
+    int j = 0;
+    int k = 0;
+
+    while(i<array->length && j<array2->length)
     {
-        array3->A[k++] = array2->A[j];
+        if(array->A[i] < array2->A[j])
+            array3->A[k++] = array->A[i++];
+        if(array->A[i] > array2->A[j])
+            array3->A[k++] = array2->A[j++];
+        else
+        {
+            array3->A[k++] = array2->A[j++];
+            i++;
+        }
     }
-    }
+    // Copy remaining elements from array1
+    while (i < array->length)
+        array3->A[k++] = array->A[i++];
+
+    while (j < array2->length)
+        array3->A[k++] = array2->A[j++];
+    
+    array3->length = k;
 }
 
 int main()
 {   
     struct Array A = {{3, 5, 10, 4, 6}, 5, 5};
     struct Array B = {{2, 4, 5, 7, 12}, 5, 5};
-    struct Array C = {{}, 10, 10};
+    struct Array C = {{}, 10, 0};
     union_unsorted(&A, &B, &C);
     display(C);
+
+    struct Array A1 = {{3, 4, 5, 6, 10}, 5, 5};
+    struct Array B1 = {{2, 4, 5, 7, 12}, 5, 5};
+    struct Array C1 = {{}, 10, 0};
+    union_sorted(&A1, &B1, &C1);
+    display(C1);    
     return 0;
 }
